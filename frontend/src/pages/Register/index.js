262d596fb81e75855eaff450bfc0a1f7e673d0a6
kiofarias <kiofarias@gmail.com>
2020-03-28 00:09:40 -0300

    conexão frontend e backend Cadastro de ONG

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,11 +1,33 @@
-import React from 'react'
-import './styles.css'
-
-import LogoImg from '../../assets/logo.svg'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
-import {FiArrowLeft} from 'react-icons/fi'
+import { FiArrowLeft } from 'react-icons/fi'
+import api from '../../services/api'
+
+import './styles.css'
+import LogoImg from '../../assets/logo.svg'
+
 
 export default function Register() {
+
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [whatsapp, setWhatsapp] = useState('')
+    const [city, setCity] = useState('')
+    const [uf, setUf] = useState('')
+
+    async function handleRegister(e) {
+        e.preventDefault()
+
+         const data = { name, email, whatsapp, city, uf }
+        console.log(data)
+        try {
+            const response = await api.post('ongs', data)
+            alert(`Seu ID de acesso: ${response.data.id} `)
+        } catch (error) {
+            alert('Erro no cadastro, tente novamente.')
+        }
+    }
+
     return (
         <div className='register-container'>
             <div className='content'>
@@ -17,21 +39,43 @@ export default function Register() {
                         a encontrarem os casos de sua ONG
                     </p>
                     <Link className='back-link' to='/'>
-                        <FiArrowLeft size={16} color='#e02041'/>
+                        <FiArrowLeft size={16} color='#e02041' />
                         Voltar para a página de logon
                     </Link>
                 </section>
-                <form>
-                    <input placeholder='Nome da ONG'/>
-                    <input type='e-mail' placeholder='E-mail'/>
-                    <input placeholder = 'WhatsApp'/>
-                <div className = 'input-group'>
-                    <input placeholder = 'Cidade'/>
-                    <input placeholder = 'UF' style={{width:80}} />
-                </div>
-                <button className='button' type='submit'>Cadastrar</button>
+                <form onSubmit={handleRegister}>
+                    <input
+                        placeholder='Nome da ONG'
+                        value={name}
+                        onChange={e => setName(e.target.value)}
+                    />
+                    <input
+                        type='e-mail'
+                        placeholder='E-mail'
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                    />
+                    <input
+                        placeholder='WhatsApp'
+                        value={whatsapp}
+                        onChange={e => setWhatsapp(e.target.value)}
+                    />
+                    <div className='input-group'>
+                        <input
+                            placeholder='Cidade'
+                            value={city}
+                            onChange={e => setCity(e.target.value)}
+                        />
+                        <input
+                            placeholder='UF'
+                            style={{ width: 80 }}
+                            value={uf}
+                            onChange={e => setUf(e.target.value)}
+                        />
+                    </div>
+                    <button className='button' type='submit'>Cadastrar</button>
                 </form>
             </div>
         </div>
-    ) 
+    )
 } 
